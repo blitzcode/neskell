9892cdb126222ca9738f32830573641e4daf8326
Tim C. Schroeder <tim@blitzcode.net>
2013-09-06 12:29:33 +0200

    Switched command line parsing to System.Console.GetOpt

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,47 @@
 
+{-# LANGUAGE LambdaCase #-}
+
 module Main (main) where
 
 import Test (runTests)
 import Instruction (disassemble)
 
-import Control.Monad (when, unless)
+import Control.Monad (when, unless, forM_)
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as B8
 import System.Environment (getArgs, getProgName)
 import System.Exit (exitSuccess, exitFailure)
+import System.Console.GetOpt
+
+data Flag = FlagTest | FlagQuickTest | FlagDAsm String
+
+options :: [OptDescr Flag]
+options = [ Option ['t'] ["test"      ] (NoArg FlagTest        ) "run full test suite"
+          , Option ['q'] ["quick-test"] (NoArg FlagQuickTest   ) "run quick (<1s) test suite"
+          , Option ['d'] ["dasm"      ] (ReqArg FlagDAsm "FILE") "disassemble binary FILE"
+          ]
 
 main :: IO ()
 main = do
-    -- TODO: This is a rather poor argument parser...
     name <- getProgName
     args <- getArgs
-    when (null args) . putStrLn $ "Usage: " ++ name ++ " --[test|quick-test] [--dasm file]"
-    when ("--test" `elem` args) $ do
-        success <- runTests False
-        unless success exitFailure
-    when ("--quick-test" `elem` args) $ do
-        success <- runTests True
-        unless success exitFailure
-    -- TODO: Add option to only run tests matching a given regex, option to list
-    --       all tests by name
-    case dropWhile (/= "--dasm") args of
-        (_:fn:_) -> B.readFile fn >>= mapM_ (B8.putStrLn) . disassemble
-        (_:_)    -> putStrLn "Missing file argument to --dasm" >> exitFailure
-        []       -> return ()
+    -- Parse command line arguments
+    let header = "Usage: " ++ name ++ " [OPTION...]"
+        usage  = usageInfo header options
+    flags <- case getOpt Permute options args of
+                 ([], [], [] ) -> putStrLn usage >> return []
+                 (f , [], [] ) -> return f
+                 (_ , _ , err) -> putStrLn (concat err ++ usage) >> return []
+    when (null flags) exitFailure
+    -- Process arguments
+    forM_ flags $ \case
+        FlagTest      -> do success <- runTests False
+                            unless success exitFailure
+        FlagQuickTest -> do success <- runTests True
+                            unless success exitFailure
+        FlagDAsm fn   -> mapM_ B8.putStrLn . disassemble =<< B.readFile fn
     exitSuccess
+
+-- TODO: Add option to only run tests matching a given regex, option to list
+--       all tests by name
 
